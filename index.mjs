9f28fe8ab2d804e28dc5b0d7c995ffd58a6dfdba
Alexangelo O. G <orozcoalexangelo@gmail.com>
2025-05-06 09:46:03 -0700

    Added CSS and SQL queries

--- index.mjs
+++ index.mjs
@@ -77,28 +77,71 @@ app.get('/addItem', isAuthenticated, (req, res) => {
     res.render('addItem.ejs');
 });
 
-app.post('/addItem', isAuthenticated, (req, res) => { //take user to wishlist for them to see the updated wishlist
+app.post('/addItem', isAuthenticated, async (req, res) => { //take user to wishlist for them to see the updated wishlist
+    let itemName = req.body.itemName;
+    let itemDesc = req.body.itemDescription;
+    let itemPrice = req.body.itemPrice;
+    let itemLink = req.body.itemLink;
+    let itemImg = req.body.itemImage;
+
+    let sql = `INSERT INTO items
+                (name, price, link, imageurl, userId)
+                VALUES
+                (?,?,?,?,?,?)`;
+    let sqlParams = [itemName,itemPrice,itemLink,itemImg,userId];
+    const[rows] = await conn.query(sql, sqlParams);
+
+    console.log(itemName + " " + itemDesc + " " + itemLink + " " + itemPrice + " " + itemImg);
     res.redirect('viewWishlist.ejs');
 });
 
-app.post('/removeItem', isAuthenticated, (req, res) => { //deletes a selected item from the wishlist
+app.post('/removeItem', isAuthenticated, async (req, res) => { //deletes a selected item from the wishlist
+    let itemId = req.body.itemId;
+
+    let sql = `DELETE FROM items WHERE itemId = ?`;
+    let sqlParams = [itemId];
+    const[rows] = await conn.query(sql, sqlParams);
+    console.log("Deleted: " + itemId);
     res.redirect('viewWishlist.ejs');
 });
 
 //user clicks items in their own wishlist to edit them
-app.get('/editItem', isAuthenticated, (req, res) => {
+app.get('/editItem', isAuthenticated, async(req, res) => {
     let itemId = req.query.itemId; //needed to know which item to edit
-    res.render('editItem.ejs');
+    let sql = `SELECT * FROM items WHERE itemId = ?`;
+    let sqlParams = [itemId];
+    const[rows] = await conn.query(sql, sqlParams);
+
+
+    res.render('editItem.ejs',{itemDetails:rows});
 });
 
-app.get('/viewWishlist', isAuthenticated, (req, res) => { //displays all items with matching userId
-    res.render('viewWishlist.ejs');
+app.get('/viewWishlist', isAuthenticated, async (req, res) => { //displays all items with matching userId
+    let selfUserId = req.session.userId;
+    let sql = `SELECT itemId, name, price, imageurl, link FROM items WHERE userId = ?`;
+    let sqlParams = [selfUserId];
+    const[rows] = await conn.query(sql, sqlParams);
+    
+    res.render('viewWishlist.ejs',{items: rows});
 });
 
-app.get('/friends', isAuthenticated, (req, res) => { //displays all friends
-    res.render('friends.ejs');
+app.get('/friends', isAuthenticated, async(req, res) => { //displays all friends
+    let selfUserId = req.session.userId;
+    let sql = `SELECT * FROM friends WHERE userid1 = ?
+                UNION
+                SELECT * FROM friends WHERE userid2= ?`;
+    let sqlParams = [userId, userId];
+    const[rows] = await conn.query(sql, sqlParams);
+    res.render('friends.ejs',{friends:rows});
 });
 
+app.get('/friendsWishlist', isAuthenticated, async(req, res) => { //displays all friends
+    let friendUserId = req.body.friendUserId;
+    let sql = `SELECT * FROM items WHERE userId = ?`;
+    let sqlParams = [friendUserId];
+    const[rows] = await conn.query(sql, sqlParams);
+    res.render('friends.ejs',{friends:rows});
+});
 
 app.get('/signOut', (req, res) => { //displays all friends
     req.session.destroy();
